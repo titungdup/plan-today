624b4696f8d92d1b0b46a6a3c9078ceb349dc0bd
Dhondup Lama <dhondup.lama@yipl.com.np>
2020-01-08 17:04:31 +0545

    Info correction

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import TextBox from "./TextBox";
-import { logo } from "./../assets/images"
+import Info from "./Info";
 
 const checkIfToday = date => {
     const today = new Date();
@@ -39,33 +39,19 @@ const App = () => {
     };
 
     const savePlans = plan => {
-        delete plan.editable
-        const updatedPlans = { ...plans, [plan.id]: plan }
+        delete plan.editable;
+        const updatedPlans = { ...plans, [plan.id]: plan };
         setPlans(updatedPlans);
         localStorage.setItem("plans", JSON.stringify(updatedPlans));
     };
 
     return (
         <>
-            <div className="info">
-                <div className="logo">
-                    <img src={logo} alt="" />
-                </div>
-                <div className="content">
-                    <p>PlanToday is a very simple and minimal planner application.</p>
-                    <p>It allows you to plan what you are going to do on a present day and review what you had planned the previous day.</p>
-                      <p>  This is not a journal or a progress tracker or tracker of any sort as it doesn’t save your any info or progress.
-                        Use it to have a clear view of you day at hand and just focus on making the best out of it without worrying about any other things.
-                        </p>
-                        <p>
-                        If you are using it on a phone, add it on you home screen to have an easy access.
-                    </p>
-                </div>
-            </div>
             <div className="app-container">
                 <Header date={date} onChange={saveDate} />
                 <TextBox plan={getPlan(date)} onChange={savePlans} />
             </div>
+            <Info />
         </>
     );
 };
